d7f7c07462414b3c955ce3736b4ab3eb878b2752
Twdor <adrianghitescu0713@gmail.com>
2022-04-16 16:02:11 +0300

    Add App.js

---
+++ store/src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./components/Navbar";
+import MainCarousel from "./components/MainCarousel";
+import ProductsSection from "./components/ProductsSection";
+import $ from 'jquery';
+
+function App() {
+  return (
+    <>
+      <Navbar />
+      <MainCarousel />
+      <ProductsSection />
+    </>
+  );
+}
+
+export default App;
